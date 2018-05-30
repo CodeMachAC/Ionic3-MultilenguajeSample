7ee641d94a2c6b2ae4a81305a7b77e874e4ea857
Angel Careaga <acareagag@gmail.com>
2018-05-29 22:11:27 -0500

    3.- Implementando funciones de traducción :sparkles:
    - Se agrego la función de traducción y se implementa mediante los botones.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// Componentes externos que realizan peticiones
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private _translate: TranslateService
+  ) {
+  }
 
+  /**
+   * Cambia el idioma mediante el servicio de @ngx-translate.
+   * @param idioma recibe idioma en formato de XX.
+   */
+  cambiaIdioma(idioma: string) {
+    console.log(`Traduzco a: ${idioma}`);
+    this._translate.use(idioma);
   }
 
 }
